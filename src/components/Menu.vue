3b713804726723bd5ebd900eb7c58306ee01b876
Vinicius Souza <bentoesouza@hotmail.com>
2021-09-28 18:32:56 -0300

    Create files .vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">Grupos</b-nav-item>
+      <b-nav-item href="#">Coração</b-nav-item>
+      <b-nav-item href="#">Perfil</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
