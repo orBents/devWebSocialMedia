2c4efcaa6b793e8e1215af697a7af3b0db69ec05
orBents <bentoesouza@hotmail.com>
2021-12-05 23:27:58 -0300

    listagem ok

--- social-view/src/views/Timeline.vue
+++ social-view/src/views/Timeline.vue
@@ -3,21 +3,10 @@
     <b-button @click="createPostModal()" variant="success">
       <b-icon icon="camera"></b-icon>
     </b-button>
-    <!--
-    <section class="container-timeline">
 
-      <post-card
-        imgSrc="https://picsum.photos/300/150/?image=41"
-        user="username"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntsed do eiusmod tempor incididunt... more "
-        likes="100"
-        postCategory="Turismo"
-      ></post-card>
+    <section v-if="errored">
+      <p>Não estamos conseguindo acessar sua timeline no momento!</p>
     </section>
-    </section>  -->
-      <section v-if="errored">
-        <p>Não estamos conseguindo acessar sua timeline no momento!</p>
-      </section>
 
     <div v-else>
       <b-alert
@@ -25,64 +14,49 @@
         dismissible
         fade
         variant="success"
-      >Evento salvo com sucesso!</b-alert>
-      <b-alert
-        :show="editModal.eventoEdited"
-        dismissible
-        fade
-        variant="warning"
-      >Evento editado com sucesso!</b-alert>
-      <b-alert
-        :show="editModal.eventoError"
-        dismissible
-        fade
-        variant="error"
-      >Erro com a operação de Evento</b-alert>
-
+        >Evento salvo com sucesso!</b-alert
+      >
+      <b-alert :show="editModal.eventoEdited" dismissible fade variant="warning"
+        >Evento editado com sucesso!</b-alert
+      >
+      <b-alert :show="editModal.eventoError" dismissible fade variant="error"
+        >Erro com a operação de Evento</b-alert
+      >
       <div v-if="loading">Carregando...</div>
 
-      <b-table v-else striped hover :items="posts" :fields="campos">
-        <template v-slot:cell(editar)="linha">
-          <b-button v-on:click="editEventoModal(linha.item)">Editar Post</b-button>
-          </template>
-      </b-table>
-    </div>
+      <section class="container-timeline">
+        <ul id="example-1">
+          <li v-for="item in posts" :key="item.postId">
+            <div>
+              <b-card
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+              >
+                <b-card-text v.text="item.title">
+                </b-card-text>
 
-    <!-- Edit modal -->
-    <b-modal
-      v-bind:id="editModal.id"
-      size="xl"
-      v-bind:title="editModal.title"
-      v-on:hide="resetEditModal"
-      ok-only
-      ok-title="Cancelar"
-    >
-      <PostCard
-        @exit="closeModal"
-        :callback="editModal.callback"
-        :modalTarget="editModal.id"
-      ></PostCard>
-    </b-modal>
-  
-</section>
+                <b-button icon="edit" variant="primary"></b-button>
+              </b-card>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
 // import axios from "axios";
-import PostCard from "@/components/PostCard";
-
 export default {
   name: "Timeline",
-  components: {
-    PostCard
-  },
   data() {
     return {
       posts: [],
       editModal: {
         id: "edit-modal",
         title: "Edite o Titulo Post",
-        image: "Edite o link da imagem",
+        image: "link da imagem",
         datePublished: "2020-11-02",
         likes: "43",
         profileId: "5",
@@ -90,10 +64,10 @@ export default {
         callback: "",
         eventoCreated: false,
         eventoEdited: false,
-        eventoError: false
+        eventoError: false,
       },
       errored: false,
-      loading: true
+      loading: true,
     };
   },
   methods: {
@@ -106,11 +80,11 @@ export default {
       this.editModal.callback = (event, data) => {
         this.$http
           .post("/api/post", data.evento_model)
-          .then(response => {
+          .then((response) => {
             //if (response.data.nome === data.editEventoData.nome) {
             console.log("Criado com sucesso!", response.data);
           })
-          .catch(error => {
+          .catch((error) => {
             console.log(error);
             this.editModal.eventoError = true;
           })
@@ -127,13 +101,13 @@ export default {
       this.editModal.callback = (event, data) => {
         this.$http
           .put("/api/post", data.evento_model)
-          .then(response => {
+          .then((response) => {
             //if (response.data.nome === data.editEventoData.nome) {
             console.log("Editado com sucesso!", response.data);
             this.editModal.eventoEdited = true;
             //}
           })
-          .catch(error => {
+          .catch((error) => {
             console.log(error);
             this.editModal.eventoError = true;
           })
@@ -150,30 +124,27 @@ export default {
     },
     closeModal() {
       this.$bvModal.hide(this.editModal.id);
-    }
+    },
   },
   mounted() {
     //axios
     this.$http
       //.get(url + eventPath)
       .get("/api/posts")
-      .then(response => {
-        response.data.forEach(item => {
-          //Campos do endereço
-          //acumulador para retornar os campos do endereço concatenados
-          this.posts.push(item);
-        });
+      .then((response) => {
+        console.log(response);
 
-        let tempCampos = Object.keys(response.data[0]);
-        this.campos = tempCampos.slice(1, 6);
-        this.campos.push("editar");
+        response.data.posts.forEach((item) => {
+          this.posts.push(item);
+          console.log("teste" + item);
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Error fetching in 'Meusposts' page: ", error);
         this.errored = true;
       })
       .finally(() => (this.loading = false));
-  }
+  },
 };
 </script>
 
